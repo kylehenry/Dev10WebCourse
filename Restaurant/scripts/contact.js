1e8738ed8b79e913a30f4d6d393f2d7e944d9348
Kyle Johnson <kylehenry@gmail.com>
2020-01-13 20:39:48 -0500

    Add validation for restaurant contact form

---
+++ Restaurant/scripts/contact.js
@@ -0,0 +1,10 @@
+function validateForm() {
+	/*
+	Bootstrap 4 handles invalid fields before submission, 
+	so the form is valid if we get here.
+	*/ 
+	
+	alert("Information is valid.");
+	
+	return false;
+}
