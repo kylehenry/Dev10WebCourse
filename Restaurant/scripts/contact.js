cbe446cc389321a47efdd209a2b4ea209886bee4
Kyle Johnson <kylehenry@gmail.com>
2020-01-19 23:07:11 -0500

    Tweak restaurant site style & add pictures

--- Restaurant/scripts/contact.js
+++ Restaurant/scripts/contact.js
@@ -1,7 +1,10 @@
 function validateForm() {
 	/*
+	Bootstrap no longer supports the method of JavaScript validation outlined
+	in the course, so validation is being handled by Bootstrap directly.
+	
 	Bootstrap 4 handles invalid fields before submission, 
-	so the form is valid if we get here.
+	so the form is valid if this function has been called.
 	*/ 
 	
 	alert("Information is valid.");
